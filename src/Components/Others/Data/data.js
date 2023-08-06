525078c8198018d1fc8c9fcc736a896682c9e06d
Aminur-Rahman-Kanon <kanon754@gmail.com>
2023-08-07 00:13:34 +0100

    client deploy 3

--- src/Components/Others/Data/data.js
+++ src/Components/Others/Data/data.js
@@ -13,16 +13,20 @@ export const productsCategory = [
         items: [
             {name: 'Apple Watch', link: 'http://localhost:3000/products/watch/apple'},
             {name: 'Garmin Watch', link: 'http://localhost:3000/products/watch/garmin'},
-            {name: 'Samsung', link: 'http://localhost:3000/products/watch/samsung'}
+            {name: 'Samsung', link: 'http://localhost:3000/products/watch/samsung'},
+            {name: 'Huawei', link: 'http://localhost:3000/products/watch/huawei'},
+            {name: 'TAG Heuer', link: 'http://localhost:3000/products/watch/tag-heuer'},
         ]
     },
     {
-        title: 'Tv',
+        title: 'Television',
         img: tv,
         items: [
             {name: 'LG', link: 'http://localhost:3000/products/tv/lg'},
             {name: 'Samsung', link: 'http://localhost:3000/products/tv/samsung'},
-            {name: 'Sony', link: 'http://localhost:3000/products/tv/sony'}
+            {name: 'Sony', link: 'http://localhost:3000/products/tv/sony'},
+            {name: 'Panasonic', link: 'http://localhost:3000/products/tv/panasonic'},
+            {name: 'Toshiba', link: 'http://localhost:3000/products/tv/toshiba'},
         ]
     },
     {
@@ -31,29 +35,35 @@ export const productsCategory = [
         items: [
             {name: 'iPad', link: 'http://localhost:3000/products/tablet/apple'},
             {name: 'Samsung', link: 'http://localhost:3000/products/tablet/samsung'},
-            {name: 'Sony', link: 'http://localhost:3000/products/tablet/sony'}
+            {name: 'Microsft', link: 'http://localhost:3000/products/tablet/microsoft'},
+            {name: 'Lenovo', link: 'http://localhost:3000/products/tablet/lenovo'},
+            {name: 'Amazon', link: 'http://localhost:3000/products/tablet/amazon'},
         ]
     },
     {
-        title: 'Phone',
+        title: 'Phones',
         img: phone,
         items: [
-            {name: 'Nintendo', link: 'http://localhost:3000/products/game/nintendo'},
-            {name: 'Playstation', link: 'http://localhost:3000/products/game/playstation'},
-            {name: 'Xbox', link: 'http://localhost:3000/products/game/xbox'}
+            {name: 'Apple', link: 'http://localhost:3000/products/phone/apple'},
+            {name: 'Samsung', link: 'http://localhost:3000/products/phone/samsung'},
+            {name: 'Asus', link: 'http://localhost:3000/products/phone/asus'},
+            {name: 'Sony', link: 'http://localhost:3000/products/phone/sony'},
+            {name: 'Pixel', link: 'http://localhost:3000/products/phone/pixel'},
         ]
     },
     {
         title: 'Games',
-        img: watch,
+        img: game,
         items: [
-            {name: 'Apple Watch', link: 'http://localhost:3000/products/watch/apple'},
-            {name: 'Garmin Watch', link: 'http://localhost:3000/products/watch/garmin'},
-            {name: 'Samsung', link: 'http://localhost:3000/products/watch/samsung'}
+            {name: 'Play Station', link: 'http://localhost:3000/products/game/ps'},
+            {name: 'Nintendo', link: 'http://localhost:3000/products/game/nintendo'},
+            {name: 'Xbox', link: 'http://localhost:3000/products/game/xbox'},
+            {name: 'Microsoft', link: 'http://localhost:3000/products/game/microsoft'},
+            {name: 'Sony', link: 'http://localhost:3000/products/game/sony'}
         ]
     },
     {
-        title: 'Laptop',
+        title: 'Laptop/Pc',
         img: laptop,
         items: [
             {name: 'Acer', link: 'http://localhost:3000/products/laptop/acer'},
@@ -61,7 +71,7 @@ export const productsCategory = [
             {name: 'Lenovo', link: 'http://localhost:3000/products/laptop/lenovo'},
             {name: 'Macbook Pro', link: 'http://localhost:3000/products/laptop/macbook-pro'},
             {name: 'Macbook Air', link: 'http://localhost:3000/products/laptop/macbook-air'},
-            {name: 'Sony', link: 'http://localhost:3000/products/laptop/sony'},
+            {name: 'Sony', link: 'http://localhost:3000/products/laptop/sony'}
         ]
     },
     {
@@ -71,7 +81,8 @@ export const productsCategory = [
             {name: 'Airpods', link: 'http://localhost:3000/products/headphones/airpods'},
             {name: 'Airpod Gen 2', link: 'http://localhost:3000/products/headphones/airpods-gen-2'},
             {name: 'Airpod Max', link: 'http://localhost:3000/products/headphones/airpod-max'},
-            {name: 'Marshal', link: 'http://localhost:3000/products/headphones/marshal'}
+            {name: 'Marshal', link: 'http://localhost:3000/products/headphones/marshal'},
+            {name: 'JBL', link: 'http://localhost:3000/products/headphones/jbl'}
         ]
     }
 ]
