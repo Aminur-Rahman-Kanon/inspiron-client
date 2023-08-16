01571407ac53f9552cb12e91bd73158b6cfc91ba
Aminur-Rahman-Kanon <kanon754@gmail.com>
2023-08-16 13:39:49 +0100

    implementing fb share 4

--- src/Components/Others/FacebookShare/facebookShare.jsx
+++ src/Components/Others/FacebookShare/facebookShare.jsx
@@ -5,21 +5,19 @@ import { faSquareFacebook } from '@fortawesome/free-brands-svg-icons';
 import { Helmet } from 'react-helmet';
 
 function FacebookShare({link, title, description, image}) {
-
     
-
     return (
         <>
-        <head>
+        <Helmet>
             <title>Inspiron</title>
             <meta name="og:type" content="website"/>
-            <meta name="og:title" content={title}/>
+            <meta name="og:title" content="e-Commerce"/>
             <meta name="og:description" content={description}/>
-            <meta name="og:image" content={image}/>
+            <meta name="og:image:url" content={image}/>
             <meta name="og:image:type" content="image/png"/>
             <meta name="og:image:width" content="600"/>
             <meta name="og:image:height" content="600"/>
-        </head>
+        </Helmet>
         <div className={styles.fbShareContainer} data-href='https://inspiron-19oa.onrender.com/' data-layout="box_count" data-size="large">
             <a target="_blank" href={`https://www.facebook.com/sharer/sharer.php?u=${link}`} className={styles.fbShareLink}>
                 <FontAwesomeIcon icon={faSquareFacebook} className={styles.fbIcon}/>
